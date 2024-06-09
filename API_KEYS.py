825cb4d7e84ad8811bf7c10fd4e2285e84fbe8a3
al3x-X-y <127189815+al3x-X-y@users.noreply.github.com>
2024-06-09 06:04:30 +0600

    Update API_KEYS.py

--- API_KEYS.py
+++ API_KEYS.py
@@ -1,5 +1,5 @@
 # API_KEYS.py
 FLATICON_API_KEY = "your_flaticon_api_key"
-GIPHY_API_KEY = "XbIwaoR4IQwNNcGMvgTdYGtB8kMq03EA"
+GIPHY_API_KEY = "your_giphy_api_key"
 PEXELS_API_KEY = "your_pexels_api_key"
 TENOR_API_KEY = "your_tenor_api_key"
